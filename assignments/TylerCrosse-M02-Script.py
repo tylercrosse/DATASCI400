5af7d4f0614789d194f12692fcc02ee36a42323b
Tyler Crosse <tylerscottcrosse@gmail.com>
2018-11-10 18:45:52 -0800

    Finish assignment

--- assignments/TylerCrosse-M02-Script.py
+++ assignments/TylerCrosse-M02-Script.py
@@ -18,7 +18,6 @@ Write a script to prepare a data set with known missing values.  The data could 
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # Read in the data from a freely available source on the internet
 def _read_data():
@@ -29,46 +28,74 @@ def _read_data():
 
 
 """
-Account for outlier values in numeric columns
+Account for outlier values in numeric column
+Removes outliers using boolean mask with limits of 2 std deviantions from mean
+
 """
-def _handle_outliers():
-    print("Handle outliers")
+def _handle_outliers(vals):
+    limit_hi = np.mean(vals) + 2*np.std(vals)
+    limit_lo = np.mean(vals) - 2*np.std(vals)
+    flag_good = (vals >= limit_lo) & (vals <= limit_hi)
+    return vals[flag_good]
 
 """
 Replace missing numeric data
 """
-def _impute_missing_numeric():
-    print("Impute missing")
+def _impute_missing_numeric(df, column_name):
+    return df[df[column_name] != "?"]
+
 
 """
-Normalize numeric values
+Z Normalize numeric values
 """
-def _normalize_values():
-    print("normalize")
+def _z_normalize_values(vals):
+    return (vals - np.mean(vals))/np.std(vals)
+
 
 """
 Bin numeric variables
+
+https://pandas.pydata.org/pandas-docs/version/0.23.4/generated/pandas.cut.html
 """
-def _bin():
-    print("bin")
+def _bin(vals, bins, labels):
+    return pd.cut(Adult["hours-per-week"], bins, labels=labels)
 
 """
 Consolidate categorical data
 """
-def _consolidate_categories():
-    print("consolidate")
+def _consolidate_education_categories(Adult):
+    Adult.loc[Adult.loc[:, "education"] == " 12th", "education"] = " HS-no-grad"
+    Adult.loc[Adult.loc[:, "education"] == " 11th", "education"] = " HS-no-grad"
+    Adult.loc[Adult.loc[:, "education"] == " 10th", "education"] = " HS-no-grad"
+    Adult.loc[Adult.loc[:, "education"] == " 9th", "education"] = " HS-no-grad"
+    Adult.loc[Adult.loc[:, "education"] == " 7th-8th", "education"] = " HS-no-grad"
+    Adult.loc[Adult.loc[:, "education"] == " 5th-6th", "education"] = " HS-no-grad"
+    Adult.loc[Adult.loc[:, "education"] == " 1st-4th", "education"] = " HS-no-grad"
+    Adult.loc[Adult.loc[:, "education"] == " Preschool", "education"] = " HS-no-grad"
+    Adult.loc[Adult.loc[:, "education"] == " Some-college", "education"] = " Post-HS"
+    Adult.loc[Adult.loc[:, "education"] == " Assoc-voc", "education"] = " Post-HS"
+    Adult.loc[Adult.loc[:, "education"] == " Assoc-acdm", "education"] = " Post-HS"
+    Adult.loc[Adult.loc[:, "education"] == " Prof-school", "education"] = " Post-HS"
+    Adult.loc[Adult.loc[:, "education"] == " Masters", "education"] = " Post-Bach"
+    Adult.loc[Adult.loc[:, "education"] == " Doctorate", "education"] = " Post-Bach"
+    return Adult
 
 """
 One-hot encode categorical data
 """
-def _one_hot():
-    print("one hot")
+def _one_hot_education(Adult):
+    Adult.loc[:, "hs-no-grad"] = (Adult.loc[:, "education"] == " HS-no-grad").astype(int)
+    Adult.loc[:, "hs-grad"] = (Adult.loc[:, "education"] == " HS-grad").astype(int)
+    Adult.loc[:, "post-hs"] = (Adult.loc[:, "education"] == " Post-HS").astype(int)
+    Adult.loc[:, "bach"] = (Adult.loc[:, "education"] == " Bachelors").astype(int)
+    Adult.loc[:, "post-bach"] = (Adult.loc[:, "education"] == " Post-Bach").astype(int)
+    return Adult
 
 """
 Remove obsolete columns
 """
-def _remove_obsolete():
-    print("remove obsolete")
+def _remove_obsolete_education(Adult):
+    return Adult.drop("education", axis=1)
 
 """
 Write dataframe to file
@@ -82,5 +109,12 @@ Main block
 """
 if __name__ == "__main__":
     Adult = _read_data()
-    print(Adult.head())
-
+    Adult["capital-gain"] = _handle_outliers(Adult["capital-gain"])
+    Adult["capital-loss"] = _handle_outliers(Adult["capital-loss"])
+    # Adult = _impute_missing_numeric(Adult,  # There is no missing numeric data
+    Adult["age"] = _z_normalize_values(Adult["age"])
+    Adult["hours-per-week"] = _bin(Adult["hours-per-week"], 3, ["part-time", "full-time", "over-time"])
+    Adult = _consolidate_education_categories(Adult)
+    Adult = _one_hot_education(Adult)
+    Adult = _remove_obsolete_education(Adult)
+    _write_to_file(Adult, "TylerCrosse-M02-Dataset.csv")
